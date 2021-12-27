98f57b14daf6825da7b3ba3394a3d3b116c8e6d1
Wells <frank31218@gmail.com>
2021-12-27 10:24:02 +0800

    新增API和撈資料呈現

---
+++ yungching/yungching.WebBase/yungChingControllerBase.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Text;
+using System.Web.Mvc;
+
+namespace yungching.WebBase
+{
+    public class yungChingControllerBase : Controller
+    {
+        protected override JsonResult Json(object data, string contentType,
+                  Encoding contentEncoding, JsonRequestBehavior behavior)
+        {
+            return new JsonNetResult()
+            {
+                Data = data,
+                ContentType = contentType,
+                ContentEncoding = contentEncoding
+            };
+        }
+    }
+}
---
+++ yungching/yungching/Controllers/EmployeesController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using yungching.Model;
+using yungching.Service.Services;
+using yungching.WebBase;
+
+namespace yungching.Controllers
+{
+    public class EmployeesController : yungChingControllerBase
+    {
+        [HttpGet]
+        public JsonResult GetEmployees()
+        {
+            IList<Employees> employees = new List<Employees>();
+            IEmployeesService employeesService = new EmployeesService();
+            employees = employeesService.GetEmployees();
+            return Json(employees);
+        }
+    }
+}
